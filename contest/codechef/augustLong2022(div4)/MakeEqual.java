2729ff4de1f7645d11676a2c077a887138c83fa6
jingru <104969882+b-knd@users.noreply.github.com>
2022-08-16 09:25:54 +0800

    Create MakeEqual.java

---
+++ contest/codechef/augustLong2022(div4)/MakeEqual.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    
+		    int min = Math.min(a, b);
+		    int max = Math.max(a, b);
+		    
+		    while(min < max){
+		        min *= 2;
+		    }
+		    
+		    if(min == max){
+		        System.out.println("YES");
+		    } else{
+		        System.out.println("NO");
+		    }
+		}
+		sc.close();
+	}
+}
