adc01ad9f0d91e53280cd016d3d420e6a55a4ceb
jingru <104969882+b-knd@users.noreply.github.com>
2022-09-12 08:55:11 +0800

    Update MostFrequentEvenElement.java

--- contest/leetcode/Weekly Contest 310/MostFrequentEvenElement.java
+++ contest/leetcode/Weekly Contest 310/MostFrequentEvenElement.java
@@ -1,16 +1,19 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
+        //keep count of the occurrences of even elements using hashmap
         for(int i: nums){
             if(i % 2 == 0){
                 hm.put(i, hm.getOrDefault(i, 0)+1);
             }
         }
         
+        //no even element found
         if(hm.size() == 0){
             return -1;
         }
         
+        //find the most frequent occurring even element
         int res = -1;
         for(int i: hm.keySet()){
             if(res == -1){
